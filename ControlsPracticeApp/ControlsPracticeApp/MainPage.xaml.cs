71899319ec73d428051e9b93827cba0d03255b69
Jaweria321 <jaweria.ehsan123@gmail.com>
2022-07-15 01:19:35 +0500

    Controls added 1. Slider 2. BoxView 3. Radio Button

--- ControlsPracticeApp/ControlsPracticeApp/MainPage.xaml.cs
+++ ControlsPracticeApp/ControlsPracticeApp/MainPage.xaml.cs
@@ -14,5 +14,39 @@ namespace ControlsPracticeApp
         {
             InitializeComponent();
         }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            SliderLabel.Text = Slider.Value.ToString();
+        }
+
+       
+
+        private void ColorRadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            //The sender argument is the RadioButton responsible for this event. You can use
+            //this to access the RadioButton object, or to distinguish between multiple
+            //RadioButton objects sharing the same CheckedChanged event handler.
+
+            RadioButton radioButton = sender as RadioButton;
+            string bg_color = radioButton.Value.ToString();
+            if( bg_color == "red" )
+            {
+                MainView.BackgroundColor = Color.Red;
+            }
+            else if( bg_color == "black")
+            {
+                MainView.BackgroundColor = Color.Black;
+            }
+            else if( bg_color == "gray")
+            {
+                MainView.BackgroundColor = Color.Gray;
+            }
+
+            
+                    
+
+   
+        }
     }
 }
